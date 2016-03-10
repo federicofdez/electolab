fc5ec4966a81a9b4e598b4016a68847b03889e68
carlosv5 <carlos.vegagarcia@hotmail.com>
2016-03-10 20:14:27 +0100

    Vista crear basica

--- js/dropdown.js
+++ js/dropdown.js
@@ -4,9 +4,25 @@
 
 $(document).ready(function(){
     $(".dropdown-menu a.dhont").on("click", function(){
-        $(".btn.dropdown-toggle").html('DHondt <span class="caret"></span>');
+        $(".sistema").html('DHondt <span class="caret"></span>');
     });
     $(".dropdown-menu a.sainte").on("click", function(){
-        $(".btn.dropdown-toggle").html('Sainte-Lagüe <span class="caret caret-up"></span>');
+        $(".sistema").html('Sainte-Lagüe <span class="caret caret-up"></span>');
     });
+    $(".dropdown-menu a.madrid").on("click", function(){
+        $("numCA").html('Madrid <span class="caret caret-up"></span>');
+    });
+    $(".dropdown-menu a.barcelona").on("click", function(){
+        $("numCA").html('Barcelona <span class="caret caret-up"></span>');
+    });
+     $(".dropdown-menu a.provincias").on("click", function(){
+        $(".circunscripciones").html('Provincias <span class="caret caret-up"></span>');
+    });
+    $(".dropdown-menu a.comunidades").on("click", function(){
+        $(".circunscripciones").html('Comunidades <span class="caret caret-up"></span>');
+    });
+     $(".dropdown-menu a.espana").on("click", function(){
+        $(".circunscripciones").html('España <span class="caret caret-up"></span>');
+    });
+
 });
