55d49ebbabec66c70c2b2fc2c7b6e2614ab82bf4
carlosv5 <carlos.vegagarcia@hotmail.com>
2016-03-31 18:51:25 +0200

    Aplicación creada

---
+++ electoLab/src/es/upm/dit/isst/electoLab20/ElectoLabServlet.java
@@ -0,0 +1,12 @@
+package es.upm.dit.isst.electoLab20;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class ElectoLabServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
