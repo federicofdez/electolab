d43891e1f546522e434476681f6de613d509ec45
FedericoFdez <fa.fernandez@alumnos.upm.es>
2016-04-17 16:41:14 +0200

    Caché añadida, servlets de crear y simular modificados

--- electoLab/src/es/upm/dit/isst/electoLab20/crearServlet.java
+++ electoLab/src/es/upm/dit/isst/electoLab20/crearServlet.java
@@ -1,12 +1,14 @@
 package es.upm.dit.isst.electoLab20;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.Enumeration;
+import java.util.UUID;
 
+import javax.cache.Cache;
+import javax.cache.CacheException;
+import javax.cache.CacheFactory;
+import javax.cache.CacheManager;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -18,96 +20,69 @@ import com.google.appengine.api.users.UserServiceFactory;
 import es.upm.dit.isst.dao.electoLabDAO;
 import es.upm.dit.isst.dao.electoLabDAOImpl;
 import es.upm.dit.isst.logica.calculos;
-import es.upm.dit.isst.model.Partido;
-import es.upm.dit.isst.model.Provincia;
+import es.upm.dit.isst.model.Escenario;
 
 @SuppressWarnings("serial")
 public class crearServlet extends HttpServlet {
-	
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
+
+		// Gestión de usuarios
 		UserService userService = UserServiceFactory.getUserService();
 		String url = userService.createLoginURL(req.getRequestURI());
 		String urlLinktext = "Login";
 		String user = "";
 		if (req.getUserPrincipal() != null) {
 			user = req.getUserPrincipal().getName();
-			System.out.println(user);
 			url = userService.createLogoutURL(req.getRequestURI());
-			System.out.println(url);
 			urlLinktext = user + " :Logout";
 		}
-
 		req.getSession().setAttribute("user", user);
 		req.getSession().setAttribute("url", url);
 		req.getSession().setAttribute("urlLinktext", urlLinktext);
 
+		// Si ha iniciado sesión pero no pertenece a ningún grupo, redirigimos
 		electoLabDAO dao = electoLabDAOImpl.getInstance();
-
 		if (user != "" && !dao.exist_usuario(user)) {
 			resp.sendRedirect("/registrar.jsp");
-		} else {
-
-			calculos calc = calculos.getInstance();
-
-			// localhost:8888/_ah/admin
-			// Prueba db Partido
-			/*
-			 * List<String> provincias2 = new ArrayList<String>(); {
-			 * provincias2.add(new String("Álava")); provincias2.add(new
-			 * String("Albacete")); } List<Double> votos2 = new
-			 * ArrayList<Double>(); { votos2.add(new Double(1212212)); }
-			 * //dao.create_partido
-			 * ("PPSOEPRUEBA","Partido Probador de todos","img/logos/psoe.png"
-			 * ,"2",provincias2,votos2,0); //System.out.println("leer" +
-			 * dao.read_partidos()); //dao.delete_partido("PPSOEPRUEBA",0);
-			 */
-
-			// Prueba db Provincia
-			// dao.create_provincia("LossstCity","Ciudad perdida","Perdida",3,
-			// 1,1);
-			// dao.create_provincia("LostCity","Ciudad perdida","Perdida",3,
-			// 1,2);
-
-			// System.out.println(dao.read_provincia("LostCity"));
-			// System.out.println(dao.read_provincias());
-			// dao.delete_provincia("LossstCity",1);
-			// System.out.println(dao.read_provincia("LostCity"));
-
-			// Prueba db Escenario
-			/*
-			 * Map<String,Integer> votos = new HashMap<String,Integer>(); {
-			 * votos.put("PP:PirateBay",99999);
-			 * votos.put("PODEMOS:Podemoslandia", 1);
-			 * 
-			 * } Map<String,Integer> escProv = new HashMap<String,Integer>(); {
-			 * escProv.put("PirateBay",5); escProv.put("Podemoslandia", 1);
-			 * 
-			 * } List<String> comentarios = new ArrayList<String>(); String[]
-			 * array = new
-			 * String[]{"Comentario1","Este escenario es una mierda enorme"
-			 * ,"Hoy"}; Gson gson = new Gson(); //convert java object to JSON
-			 * format String json = gson.toJson(array); comentarios.add(json);
-			 * 
-			 * System.out.println(json);
-			 * dao.create_escenario("Carlos",votos,escProv
-			 * ,"DHont","España",150,comentarios);
-			 * 
-			 * //System.out.println(dao.read_escenario(1));
-			 * System.out.println(dao.read_escenarios());
-			 * //dao.delete_escenario(1);
-			 * //System.out.println(dao.read_escenarios());
-			 */
-
-			// Habría que hacerlo con el dao
-			req.getSession().setAttribute("provincias",
-					dao.read_escenario("admin").getProvincias());
-			req.getSession().setAttribute("partidos",
-					dao.read_escenario("admin").getPartidos());
-
-			resp.sendRedirect("/crear.jsp");
+			return;
 		}
+		
+		req.getSession().setAttribute("provincias",
+				dao.read_escenario("admin").getProvincias());
+		req.getSession().setAttribute("partidos",
+				dao.read_escenario("admin").getPartidos());
+		resp.sendRedirect("/crear.jsp");
 	}
 
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		electoLabDAO dao = electoLabDAOImpl.getInstance();
+		Enumeration em = req.getParameterNames();
+		int i = 0;
+		String[] datos = new String[req.getContentLength()];
+		while (em.hasMoreElements()) {
+			String paraName = (String) em.nextElement();
+			datos[i] = req.getParameter(paraName);
+			i++;
+		}
+
+		calculos calc = calculos.getInstance();
+		Escenario escenario = calc.crearEscenario(em, datos);
+
+		Cache cache;
+		try {
+			CacheFactory cacheFactory = CacheManager.getInstance()
+					.getCacheFactory();
+			cache = cacheFactory.createCache(Collections.emptyMap());
+			String escenarioKey = UUID.randomUUID().toString();
+			cache.put(escenarioKey, escenario);
+			resp.sendRedirect("/simular?escenario=" + escenarioKey);
+		} catch (CacheException e) {
+			e.printStackTrace();
+		}
+	}
 }
--- electoLab/src/es/upm/dit/isst/electoLab20/simularServlet.java
+++ electoLab/src/es/upm/dit/isst/electoLab20/simularServlet.java
@@ -1,8 +1,15 @@
 package es.upm.dit.isst.electoLab20;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.Enumeration;
+import java.util.UUID;
 
+import javax.cache.Cache;
+import javax.cache.CacheException;
+import javax.cache.CacheFactory;
+import javax.cache.CacheManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -14,12 +21,14 @@ import com.google.appengine.api.users.UserServiceFactory;
 import es.upm.dit.isst.dao.electoLabDAO;
 import es.upm.dit.isst.dao.electoLabDAOImpl;
 import es.upm.dit.isst.logica.calculos;
+import es.upm.dit.isst.model.Escenario;
 
 public class simularServlet extends HttpServlet {
 
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
+		// Gestión de usuarios
 		UserService userService = UserServiceFactory.getUserService();
 		String url = userService.createLoginURL(req.getRequestURI());
 		String urlLinktext = "Login";
@@ -29,74 +38,30 @@ public class simularServlet extends HttpServlet {
 			url = userService.createLogoutURL(req.getRequestURI());
 			urlLinktext = user + " :Logout";
 		}
-
 		req.getSession().setAttribute("user", user);
 		req.getSession().setAttribute("url", url);
 		req.getSession().setAttribute("urlLinktext", urlLinktext);
 
+		// Si ha iniciado sesión pero no pertenece a ningún grupo, redirigimos
 		electoLabDAO dao = electoLabDAOImpl.getInstance();
 		if (user != "" && !dao.exist_usuario(user)) {
 			resp.sendRedirect("/registrar.jsp");
-		} else {
-
-			Enumeration em = req.getParameterNames();
-			// int total_votos=0;
-			int i = 0;
-			String[] datos2 = new String[req.getContentLength()];
-			while (em.hasMoreElements()) {
-				String paraName = (String) em.nextElement();
-				// total_votos = calculos.total_votos(req.getParameterNames(),
-				// req.getParameter(paraName));
-				// if(req.getParameter(paraName) != "" && paraName.indexOf("PP")
-				// != -1 ){
-				// total_votos += Integer.parseInt(req.getParameter(paraName));
-				// System.out.println(paraName + "=" +
-				// req.getParameter(paraName) );
-				datos2[i] = req.getParameter(paraName);
-				// if(paraName.indexOf("sistema") != -1){
-				// System.out.println(req.getParameter(paraName));
-				// }
-				// if(paraName.indexOf("avila") != -1){
-				// System.out.println(req.getParameter(paraName));
-				// }
-				// System.out.println(paraName + datos2[i]);
-				i++;
-				// req.setAttribute("total_votos", total_votos);
-				// }
-			}
-
-			calculos calc = calculos.getInstance();
-			// System.out.println(calc.calcula_esca(req.getParameterNames(),
-			// datos2));
-			for (int w = 0; w < 10; w++) {
-				// System.out.println("Total escaños " +
-				// calc.metodo_dhont_Avila(req.getParameterNames(), datos2)[w]);
-
-			}
-			// if(calc.porcentaje_correctos(req.getParameterNames(), datos2)){
-			// System.out.println("Porcentajes correctos");
-			// }else{
-			// System.out.println("Cuidado porcentajes mal ajustados");
-			// }
-
-			// String votos =
-			// String.valueOf(calc.total_votos(req.getParameterNames(),
-			// datos2));
-
-			// List<Partido> votosTabla =
-			// calc.calculaVotos(req.getParameterNames(), datos2);
-			// req.getSession().setAttribute("votos", votosTabla);
-			// System.out.println(req.getParameter("mayoria"));
-
-			// calc.calcula_esc(calc.crea_mapa(req.getParameterNames(),
-			// datos2));
-			// calc.crea_mapa(req.getParameterNames(), datos2);
-			// calc.esca_map(req.getParameterNames(), datos2);
-
-			// System.out.println("votos totales = " + votos);
-			resp.sendRedirect("/simular.jsp");
+			return;
 		}
-
+		
+		if (req.getParameterMap().containsKey("escenario")){
+			Cache cache;
+			try {
+				CacheFactory cacheFactory = CacheManager.getInstance()
+						.getCacheFactory();
+				cache = cacheFactory.createCache(Collections.emptyMap());
+				Escenario escenario = (Escenario) cache.get(req.getAttribute("escenario"));
+				req.setAttribute("escenario", escenario);
+			} catch (CacheException e) {
+				e.printStackTrace();
+			}
+		}
+		req.getRequestDispatcher("simular.jsp").forward(req, resp);
 	}
 
 }
